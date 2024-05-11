6b9d7190590dbe2f21d9fea59cc0ce430849b278
Máximo <maximogoitia277@gmail.com>
2024-05-11 12:48:55 -0300

    login-register en 1920px

---
+++ js/login-register.js
@@ -0,0 +1,9 @@
+function mostrar_login() {
+    document.querySelector('.register-contenedor').style.display = 'none';
+    document.querySelector('.login-contenedor').style.display = 'flex';
+}
+
+function mostrar_register() {
+    document.querySelector('.login-contenedor').style.display = 'none';
+    document.querySelector('.register-contenedor').style.display = 'flex';
+}
