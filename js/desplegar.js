240bb65cb4a6517a8521ad7f0bb9107273790a81
Máximo <maximogoitia277@gmail.com>
2024-05-23 20:22:12 -0300

    sobre nosotros terminado (sin info)

--- js/desplegar.js
+++ js/desplegar.js
@@ -1,27 +1,27 @@
 function desplegar(contentId) {
     var contents = document.getElementsByClassName('desplegable');
-    var arrows = document.getElementsByClassName('downarrow');
+    var arrows = document.getElementsByClassName('arrow');
     
+    // Cerrar todos los contenidos y mostrar todas las flechas
     for (var i = 0; i < contents.length; i++) {
-        if (contents[i].id !== contentId) {
-            contents[i].style.display = 'none';
-        }
+        contents[i].style.display = 'none';  // Ocultar contenido
     }
 
     for (var j = 0; j < arrows.length; j++) {
-        if (arrows[j].id !== 'downarrow' + contentId.slice(-1)) {
-            arrows[j].classList.remove('hidden');
-        }
+        arrows[j].classList.remove('hidden');  // Mostrar flecha
     }
     
+    // Obtener el contenido actual y la flecha correspondiente
     var content = document.getElementById(contentId);
     var arrow = document.getElementById('downarrow' + contentId.slice(-1));
 
+    // Alternar la visibilidad del contenido actual y la flecha correspondiente
     if (content.style.display === "flex") {
-        content.style.display = "none";
-        arrow.classList.remove('hidden');
+        content.style.display = "none";  // Ocultar contenido
+        arrow.classList.remove('hidden');  // Mostrar flecha
     } else {
-        content.style.display = "flex";
-        arrow.classList.add('hidden');
+        content.style.display = "flex";  // Mostrar contenido
+        arrow.classList.add('hidden');  // Ocultar flecha
     }
 }
+
