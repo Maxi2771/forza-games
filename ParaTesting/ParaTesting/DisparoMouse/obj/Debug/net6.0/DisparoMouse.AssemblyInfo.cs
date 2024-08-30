c44ae43951d3ab72614715dcd7f3bf45962e24ed
Fabrizio-Amado <blockzkid@gmail.com>
2024-08-30 19:45:55 -0300

    Agrego funcionalidad arma

--- ParaTesting/ParaTesting/DisparoMouse/obj/Debug/net6.0/DisparoMouse.AssemblyInfo.cs
+++ ParaTesting/ParaTesting/DisparoMouse/obj/Debug/net6.0/DisparoMouse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisparoMouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93ec351980e21708ef3cf0f506e9eb45fc42a43")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisparoMouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisparoMouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
